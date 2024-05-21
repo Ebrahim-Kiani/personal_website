afcadb994ef72e431a8f18f024aedcb837df7164
kianiebrahim <ebimp4@gmail.com>
2024-05-22 01:54:22 +0330

    crete admin function is complited.
    now it's ready to make admin login.

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from datetime import datetime
+from getpass import getpass
+from password_strength import PasswordPolicy
+from flask import Flask, request, flash, render_template
+from flask_bcrypt import Bcrypt
 from home_module.home import home_bp
 from flask_migrate import Migrate
-from home_module.models import db
+from flask_login import LoginManager, current_user, UserMixin, login_user, logout_user
+from flask_admin import Admin, AdminIndexView
+from flask_admin.contrib.sqla import ModelView
+from validate_email import validate_email
+from models import User, db
+
 import os
 
 app = Flask(__name__)
@@ -11,6 +20,10 @@ app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'mydatabase.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'mysecret'
+app.config['SECURITY_PASSWORD_HASH'] = 'bcrypt'
+app.config['SECURITY_PASSWORD_SALT'] = b'$2b$12$wqKlYjmOfXPkjfhksfhsk.'  # custom salt
+
 db.init_app(app)  # Initialize SQLAlchemy
 
 # Define modules here
@@ -19,7 +32,102 @@ app.register_blueprint(home_bp, url_prefix='')
 # Define model(s) Migrate here
 migrate = Migrate(app, db)
 
-# Other routes and configurations
+# Define login here
+login = LoginManager(app)
+
+
+@login.user_loader
+def load_user(user_id):
+    return User(user_id)
+
+
+@app.route("/admin/login", methods=['GET', 'POST'])
+def admin_login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = User.get(email=email)
+        if user:
+            hashed_password = user.password
+            is_valid = bcrypt.check_password_hash(hashed_password, password)
+            if is_valid:
+                load_user(user)
+                flash('Logged in successfully!', 'success')
+
+        else:
+            flash('Invalid credentials', 'danger')
+    return render_template('login.html')
+
+
+# Define bcrypt
+bcrypt = Bcrypt(app)
+
+policy = PasswordPolicy.from_names(
+    length=8,
+    uppercase=1,
+    numbers=1,
+    special=1,
+    nonletters=1
+)
+
+
+# Define admin creator
+@app.cli.command("create_admin")
+def create_admin():
+    fullname = input("Enter admin's full name: ")
+    email = input("Enter admin's email: ")
+
+    if not validate_email(email):
+        print("invalid email(your email must be like email@example.com)")
+        return 0
+
+    password = str(getpass("Enter admin's password: "))
+    confirm_password = str(getpass("Enter admin's confrim password:"))
+
+    # Test strong of password
+    result = policy.test(password)
+
+    if confirm_password != password:
+        print("password and confirm passwords do not match")
+        return 0
+
+    user_answer = str()
+    if len(result) != 0:
+        print("Your password is not secure and must meet the minimum requirements below:")
+        print(result)
+        user_answer = input("Do you want to continue?(Y/n):")
+
+        if user_answer == "n":
+            return 0
+
+    elif len(user_answer) == 0 or user_answer == "Y":
+
+        # Generate a hash of the password
+        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+        # Create the admin user object
+        admin_user = User(fullname=fullname, email=email, password=hashed_password, is_admin=True, is_active=True)
+
+        # Add the user object to the database session and commit the changes
+        db.session.add(admin_user)
+        db.session.commit()
+        print("Admin user created successfully.")
+
+
+# Set Admin session
+class MyModelView(ModelView):
+    def is_accessible(self):
+        return current_user.is_authenticated
+
+
+class MyAdminIndexView(AdminIndexView):
+    def is_accessible(self):
+        return current_user.is_authenticated and current_user.is_admin
+
+
+# Define admin here
+admin = Admin(app, index_view=MyAdminIndexView())
+admin.add_view(MyModelView(User, db.session))
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- models.py
+++ models.py
@@ -1,25 +1,23 @@
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import CheckConstraint
+
 
 db = SQLAlchemy()
 class User(db.Model):
     __tablename__ = 'users'  # Table name (optional)flask db migrate -m "Initial migration"
     id = db.Column(db.Integer(), primary_key=True)
     fullname = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
     phone = db.Column(db.Text(), nullable=True)
     city = db.Column(db.String(50), nullable=True)
     DateOfBirth = db.Column(db.DateTime, nullable=True)
     job = db.Column(db.String(50), nullable=True)
     AboutMe = db.Column(db.Text, nullable=True)
-    is_admin = db.Column(db.Boolean(), default=False)
-    is_active = db.Column(db.Boolean(), default=True)
+    is_admin = db.Column(db.Boolean(), default=False, unique=True, nullable=False)
+    is_active = db.Column(db.Boolean(), default=True, nullable=False)
     created_on = db.Column(db.DateTime(), default=datetime.utcnow)
     updated_on = db.Column(db.DateTime(), default=datetime.utcnow, onupdate=datetime.utcnow)
-    # Add a CheckConstraint to ensure only one row has is_admin=True
-    __table_args__ = (
-        CheckConstraint('is_admin = (is_admin IS NOT NULL)', name='check_single_admin'),
-    )
+
     def __repr__(self):
         return f"<User {self.id}: {self.fullname}>"
